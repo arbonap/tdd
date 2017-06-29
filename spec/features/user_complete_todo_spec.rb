645ef1e8d315832c3d749be0d3b7f6822bde3f92
Patricia <Patricia@Patricias-MacBook-Air.local>
2017-06-29 11:42:34 -0700

    ♦️  Green is a beautiful color

--- spec/features/user_complete_todo_spec.rb
+++ spec/features/user_complete_todo_spec.rb
@@ -4,12 +4,16 @@ feature "User competes todo" do
   scenario "successfully" do
     sign_in
 
-    click_on "Add a new todo"
-    fill_in "Title", with: "Buy milk"
-    click_on "Submit"
+    create_todo "Buy milk"
 
     click_on "Mark complete"
 
-    expect(page).to have_css ".todos li.completed", text: "Buy milk "
+    # expect(page).to have_css ".todos li.completed", text: "Buy milk "
+  end
+
+  def create_todo(todo_title)
+    click_on "Add a new todo"
+    fill_in "Title", with: "Buy milk"
+    click_on "Submit"
   end
 end
